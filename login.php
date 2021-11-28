a278958012e5aa044cc990960c9972e6a79453f2
Katlego <40573286+Kpmahlaole@users.noreply.github.com>
2021-11-28 12:34:26 +0200

    Update login.php
    Login page enhanced

--- login.php
+++ login.php
@@ -93,16 +93,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
-    </style>
+      <link rel="stylesheet" href="style/style.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
 </head>
-<body>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
+<div class="body"></div>
+        <div class="grad"></div>
+        <div class="header">
+            <div>Genesis<span><b>PRO</b></span></div>
+        </div>
+        <br>
+		
+	<div class="login">
 
         <?php
         if(!empty($login_err)){
@@ -111,21 +113,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         ?>
 
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
+            
+			<div class="form-group">
                 <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
             </div>
+			
             <div class="form-group">
-                <label>Password</label>
                 <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
+			
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+			
+            
         </form>
+		
     </div>
 </body>
 </html>
