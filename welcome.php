edf1b444d4882e3a882d2ec77bc7a123ccac2a69
Katlego <40573286+Kpmahlaole@users.noreply.github.com>
2021-11-28 12:51:32 +0200

    Update welcome.php
    button naming fix

--- welcome.php
+++ welcome.php
@@ -51,7 +51,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </form>
 	
 	<p>
-        <a href="new_policy.php" class="btn btn-success">Reset Your Password</a>
+        <a href="new_policy.php" class="btn btn-success">Capture New Policy</a>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
     </p>
