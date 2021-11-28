4af2053e55f5b208cffde2fe63b0092a2d312aa3
Katlego <40573286+Kpmahlaole@users.noreply.github.com>
2021-11-28 13:18:21 +0200

    Update new_policy.php

--- new_policy.php
+++ new_policy.php
@@ -1,224 +1,186 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
+    <meta name="generator"
+    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Bootstrap CSS -->
-<!--     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!--     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
  -->
-   <title>Capture Policy</title>
+    <title>Capture Policy</title>
   </head>
   <body>
-  
   <div class="container">
-    <form id="register" name="new_policy" action="welcome.php" method="POST" enctype="multipart/form-data" target="_self">
-        
-        <div class="form-group">
-       
-
-
-
-
-      
-<br>
-Cover details (Please select your cover option).
-
-
-<div class="input-group">
-<span class="input-group-text"><b>Plan options<b><br></span>
-Option 1<input id="option1" name="option1" type="radio" value="option1">
-Option 2<input id="option1" name="option1" type="radio" value="option1">
-Option 3<input id="option1" name="option1" type="radio" value="option1">
-Option 4<input id="option1" name="option1" type="radio" value="option1">
-</div>
-
-<div class="input-group">
-<span class="input-group-text"><b><b><br></span>
-<input id="name" name="name" placeholder="Title"> 
-<input id="name" name="name" placeholder="Initials">
-</div>
-
-<div class="input-group">
-<span class="input-group-text"><b><b><br></span>
-<input id="name" name="name" placeholder="Surname">
-<input id="name" name="name" placeholder="First names">
-<input id="name" name="name" placeholder="Gender">
-<input id="name" name="name" placeholder="Identity number"><br><br>
-</div>
-
-
-
-<b>Contact Information</b><br>
-<input id="name" name="name" type="text" placeholder="Telephone home">
-<input id="name" name="name" type="phone" placeholder="Cell">
-<input id="name" name="name" type="phone" placeholder="Telephone work">
-<input id="name" name="name" type="email" placeholder="Email">
-<input id="name" name="name" type="dropdown" placeholder="Preferred method of communication Tel Cell Email Post">
-<br><br>
-
-<b>Address Information</b><br>
-<div class="input-group">
-  <span class="input-group-text"></span>
-  <textarea class="form-control" aria-label="With textarea" placeholder="Residential address"></textarea>
-</div>
-<input id="name" name="name" type="text" placeholder="Postal code">
-
-<br><br>
-Child dependent details<br> 
-<!-- (Must be younger than age 21 at the time of application, up to a maximum of six children on your policy) -->
-
-<div class="input-group">
-  <span class="input-group-text">1.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">2.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">3.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">4.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">5.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">6.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">7.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">8.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">9.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-
-
-
-
-<br><br>
-Extended family member details<br>
-(Members limited to a maximum of ten between ages 0 - 65 years and 66 – 85 years) Age refers to age next birthday<br>
-<div class="input-group">
-  <span class="input-group-text">1.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">2.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">3.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">4.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<div class="input-group">
-  <span class="input-group-text">5.</span>
-  <input id="name" name="name" placeholder="Surname">
-  <input id="name" name="name" placeholder="Full names"> 
-  <input id="name" name="name" placeholder="Date of birth/identity number ">
-  <input id="name" name="name" placeholder="Relationship to main member">
-</div>
-
-<br><br>
-<b>Upload Documents</b><br>
-<label>Document 1</label>
-<input id="name" name="name" placeholder="Placeholder" type="file"><br>
-<label>Document 2</label>
-<input id="name" name="name" placeholder="Placeholder" type="file"><br>
-<label>Document 3</label>
-<input id="name" name="name" placeholder="Placeholder" type="file">
-<br><br>
-
-
-
-<div class="input-group">
-  <span class="input-group-text"></span>
-  <input id="submit" name="submit" type="submit" value="submit">
-</div>
-</form>
-
-</div>
-  
-          
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+    <form id="register" name="new_policy" action="welcome.php" method="post" enctype="multipart/form-data" target="_self">
+      <div class="form-group">
+      <br />Cover details (Please select your cover option).
+      <div class="input-group">
+      <span class="input-group-text">
+        <b>Plan options</b>
+        <br />
+      </span> Option 1
+      <input id="option1" name="option1" type="radio" value="option1" /> Option 2
+      <input id="option1" name="option1" type="radio" value="option1" /> Option 3
+      <input id="option1" name="option1" type="radio" value="option1" /> Option 4
+      <input id="option1" name="option1" type="radio" value="option1" /></div>
+      <div class="input-group">
+        <span class="input-group-text">
+        <b>
+          <br />
+        </b> 
+        <b>
+        <input id="name" name="name" placeholder="Title" /> 
+        <input id="name" name="name" placeholder="Initials" /></b></span>
+      </div>
+      <div class="input-group">
+        <b>
+          <span class="input-group-text">
+            <b>
+            <br /> 
+            <input id="name" name="name" placeholder="Surname" /> 
+            <input id="name" name="name" placeholder="First names" /> 
+            <input id="name" name="name" placeholder="Gender" /> 
+            <input id="name" name="name" placeholder="Identity number" />
+            <br />
+            <br /></b>
+          </span>
+        </b>
+      </div>
+      <b>Contact Information
+      <br />
+      <input id="name" name="name" type="text" placeholder="Telephone home" /> 
+      <input id="name" name="name" type="phone" placeholder="Cell" /> 
+      <input id="name" name="name" type="phone" placeholder="Telephone work" /> 
+      <input id="name" name="name" type="email" placeholder="Email" /> 
+      <input id="name" name="name" type="dropdown" placeholder="Preferred method of communication Tel Cell Email Post" />
+      <br />
+      <br />Address Information
+      <br /></b>
+      <div class="input-group">
+        <textarea class="form-control" aria-label="With textarea" placeholder="Residential address"></textarea>
+      </div>
+      <input id="name" name="name" type="text" placeholder="Postal code" />
+      <br />
+      <br />Child dependent details
+      <br />
+      <!-- (Must be younger than age 21 at the time of application, up to a maximum of six children on your policy) -->
+      <div class="input-group">
+      <span class="input-group-text">1.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">2.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">3.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">4.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">5.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">6.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">7.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">8.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">9.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <br />
+      <br />Extended family member details
+      <br />(Members limited to a maximum of ten between ages 0 - 65 years and 66 – 85 years) Age refers to age next birthday
+      <br />
+      <div class="input-group">
+      <span class="input-group-text">1.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">2.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">3.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">4.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <div class="input-group">
+      <span class="input-group-text">5.</span> 
+      <input id="name" name="name" placeholder="Surname" /> 
+      <input id="name" name="name" placeholder="Full names" /> 
+      <input id="name" name="name" placeholder="Date of birth/identity number" /> 
+      <input id="name" name="name" placeholder="Relationship to main member" /></div>
+      <br />
+      <br />
+      <b>Upload Documents</b>
+      <br />
+      <label>Document 1</label> 
+      <input id="name" name="name" placeholder="Placeholder" type="file" />
+      <br />
+      <label>Document 2</label> 
+      <input id="name" name="name" placeholder="Placeholder" type="file" />
+      <br />
+      <label>Document 3</label> 
+      <input id="name" name="name" placeholder="Placeholder" type="file" />
+      <br />
+      <br />
+      <div class="input-group">
+        <input id="submit" name="submit" type="submit" value="submit" />
+      </div></div>
+    </form>
+  </div>
+  <!-- Optional JavaScript; choose one of the two! -->
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+   
+  <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  </body>
+    --></body>
 </html>
